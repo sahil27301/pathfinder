649aa8205706ba63f62d8ee0b4b5026f5cef7707
sahil27301 <65967824+sahil27301@users.noreply.github.com>
2020-08-12 16:25:44 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,220 @@
+var addingWalls=false;
+var removingWalls=false;
+var solving=false;
+var flag=false;
+$('.solving').hide();
+
+var startCoordinates='0-0';
+var endCoordinates='21-49';
+var columns=50;
+var rows=22;
+var startX=parseInt(startCoordinates.split('-')[0]);
+var startY=parseInt(startCoordinates.split('-')[1]);
+var destinationX=parseInt(endCoordinates.split('-')[0]);
+var destinationY=parseInt(endCoordinates.split('-')[1]);
+var currentX, currentY;
+var speed=15;
+
+$('.mazeData').mousedown(function(){
+  if((!$(this).hasClass(startCoordinates)) && (!$(this).hasClass(endCoordinates)) && !solving && !$(this).hasClass('wall')){
+    $(this).addClass('wall');
+    addingWalls=true;
+  }else if ((!$(this).hasClass(startCoordinates)) && (!$(this).hasClass(endCoordinates)) && !solving && $(this).hasClass('wall')) {
+    $(this).removeClass('wall');
+    removingWalls=true;
+  }
+});
+
+$('.speed').change(function(){
+  if (!solving) {
+    speed=parseFloat($(this).val());
+  }
+});
+
+$(document).mouseup(function(){
+  // console.log('mouseup occured');
+  if (addingWalls) {
+    addingWalls=false;
+  }
+  if (removingWalls) {
+    removingWalls=false;
+    // console.log('removing walls toggled');
+  }
+});
+
+$('.maze').mousemove(function(event){
+  event.preventDefault();
+});
+
+$('.mazeData').mousemove(function(event){
+  event.preventDefault();
+  if (addingWalls && (!$(this).hasClass(startCoordinates)) && (!$(this).hasClass(endCoordinates)) && !solving) {
+    $(this).addClass('wall');
+  }else if (removingWalls && (!$(this).hasClass(startCoordinates)) && (!$(this).hasClass(endCoordinates)) && !solving) {
+    $(this).removeClass('wall');
+  }
+});
+
+$('.clear').click(function(){
+  if(!solving){
+    $('.'+endCoordinates).removeClass('success');
+    $('.mazeData').each(function(){
+      $(this).removeClass('wall');
+      $(this).removeClass('active');
+    });
+  }
+});
+
+$('.'+startCoordinates).addClass('startPoint');
+$('.'+endCoordinates).addClass('endPoint');
+
+
+var stack=['start']
+var maze=[];
+
+
+var delay=1800;
+
+function createArray(){
+  $('.mazeData').each(function(){
+    $(this).removeClass('active');
+  });flag=false;
+  maze=[];
+  delay=1800;
+  currentX=startX;
+  currentY=startY;
+  for (var i = 0; i < rows; i++) {
+    maze.push([]);
+    for (var j = 0; j < columns; j++) {
+      if ($('.'+i+'-'+j).hasClass('wall')) {
+        maze[i].push(1)
+      }else {
+        maze[i].push(0)
+      }
+    }
+  }
+}
+function activate(currentX, currentY){
+  if (!(currentX==startX && currentY==startY) && !(currentX==destinationX && currentY==destinationY)) {
+    $('.'+currentX+'-'+currentY).addClass('active');
+    // $('.'+currentX+'-'+currentY).addClass('flash');
+    // setTimeout(function(){
+    //   $('.'+currentX+'-'+currentY).removeClass('flash');
+    // }, 25);
+  }
+}
+
+function deactivate(currentX, currentY){
+  if (!(currentX==startX && currentY==startY) && !(currentX==destinationX && currentY==destinationY)) {
+    $('.'+currentX+'-'+currentY).removeClass('active');
+  //   $('.'+currentX+'-'+currentY).addClass('flash');
+  //   setTimeout(function(){
+  //     $('.'+currentX+'-'+currentY).removeClass('flash');
+  //   }, 25);
+  }
+}
+
+function addDelay(currentX, currentY, delay){
+  setTimeout(function(){
+    activate(currentX, currentY);
+  }, delay);
+}
+function removeDelay(currentX, currentY, delay){
+  setTimeout(function(){
+    deactivate(currentX, currentY);
+  }, delay);
+}
+
+function findPath(){
+  maze[currentX][currentY]=2;
+  addDelay(currentX, currentY, delay);
+  delay+=speed;
+  if (currentX==destinationX && currentY==destinationY){
+    flag=1;
+    return;
+  }
+  //try going right, if you haven't come from the right and it isnt' visited and it's inside the board
+  if(stack[stack.length-1]!='left' && currentY!=columns-1 && maze[currentX][currentY+1]!=1 && maze[currentX][currentY+1]!=2)
+  {
+    currentY+=1;
+    stack.push("right");
+    findPath();
+    currentY-=1;
+    if(flag)
+    {
+        return;
+    }
+    stack.pop();
+  }
+  //Same way for down
+  if(stack[stack.length-1]!='up' && currentX!=rows-1 && maze[currentX+1][currentY]!=1 && maze[currentX+1][currentY]!=2)
+  {
+    currentX+=1;
+    stack.push("down");
+    findPath();
+    currentX-=1;
+    if(flag)
+    {
+        return;
+    }
+    stack.pop();
+  }
+  //Same way for left
+  if(stack[stack.length-1]!='right' && currentY!=0 && maze[currentX][currentY-1]!=1 && maze[currentX][currentY-1]!=2)
+  {
+    currentY-=1;
+    stack.push("left");
+    findPath();
+    currentY+=1;
+    if(flag)
+    {
+        return;
+    }
+    stack.pop();
+  }
+  //Same way for up
+  if(stack[stack.length-1]!='down' && currentX!=0 && maze[currentX-1][currentY]!=1 && maze[currentX-1][currentY]!=2)
+  {
+    currentX-=1;
+    stack.push("up");
+    findPath();
+    currentX+=1;
+    if(flag)
+    {
+        return;
+    }
+    stack.pop();
+  }
+  removeDelay(currentX, currentY, delay);
+  delay+=speed;
+}
+
+
+$('.find').click(function(){
+  if(!solving){
+    $('.'+endCoordinates).removeClass('success');
+    $('.find').text('FINDING')
+    setTimeout(function(){
+      $('.find').text($('.find').text()+'.')
+    }, 600);
+    setTimeout(function(){
+      $('.find').text($('.find').text()+'.')
+    }, 1200);
+    setTimeout(function(){
+      $('.find').text($('.find').text()+'.')
+    }, 1800);
+    solving=true;
+    createArray();
+    findPath();
+    if (!flag) {
+      setTimeout(function(){
+        alert("No solution!");
+      }, delay);
+    }
+    setTimeout(function(){
+      solving=false;
+      $('.'+endCoordinates).addClass('success');
+      $('.find').text('FIND')
+    }, delay);
+  }
+});
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>pathfinding</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <div class="container-fluid heading">
+      <h1>Sahil's Pathfinding Visualiser</h1>
+    </div>
+    <!-- <div class="container-fluid instructions">
+      <h3>How to</h3>
+      <ul>
+        <li>This page will help visualise a DFS pathfinding algorithm.</li>
+        <li>You can set the start point, end point, and walls below.</li>
+        <li>Walls will not allow a path to go through them.</li>
+        <li>Click on find to start searching for a path.</li>
+      </ul>
+    </div> -->
+    <div class="container-fluid maze">
+      <div class='mazeControl'>
+        <button type="button" class="find btn btn-primary btn-md">FIND</button>
+        <button type="button" class="clear btn btn-primary btn-md">CLEAR BOARD</button>
+        <span class='speedControl'>
+          <label>Select the animation speed:</label>
+          <select class="speed">
+            <option value="1">INSANE</option>
+            <option value="10">FAST</option>
+            <option value="16" selected>NORMAL</option>
+            <option value="23">SLOW</option>
+            <option value="30">SNAIL</option>
+          </select>
+        </span>
+      </div>
+      <p class='solving'>Solving!</p>
+      <table class='mazeTable'>
+        <?php
+          for ($i=0; $i < 22; $i++) {
+            echo "<tr>";
+              for ($j=0; $j < 50; $j++) {
+                echo "<td class='mazeData ".$i."-".$j."'></td>";
+              }
+            echo "</tr>";
+          }
+        ?>
+      </table>
+    </div>
+    <script src="index.js" charset="utf-8"></script>
+  </body>
+</html>
